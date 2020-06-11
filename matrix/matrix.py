238361db14830906ddc9f4929b6fa305ee55984d
adrianoff <adrianov87@gmail.com>
2020-06-11 18:30:13 +0300

    matrix

--- matrix/matrix.py
+++ matrix/matrix.py
@@ -1,9 +1,18 @@
+import numpy as np
+
+
 class Matrix:
-    def __init__(self, matrix_string):
-        pass
+    def __init__(self, s):
+        self.matrix = np.array([col for col in [list(map(int, row.split())) for row in s.split("\n")]])
 
     def row(self, index):
-        pass
+        return self.matrix[index-1, :]
 
     def column(self, index):
-        pass
+        return self.matrix[:, index-1]
+
+
+m = Matrix("1 2 3\n5 56 7\n9 2 6")
+print(m.column(0))
+
+#[print(col) for col in [row.split() for row in s.split("\n")]]

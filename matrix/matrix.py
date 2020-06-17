988d032c3e96bdd13b7d04d78a96772209345d46
adrianoff <adrianov87@gmail.com>
2020-06-17 19:45:32 +0300

    hamming

--- matrix/matrix.py
+++ matrix/matrix.py
@@ -6,13 +6,7 @@ class Matrix:
         self.matrix = np.array([col for col in [list(map(int, row.split())) for row in s.split("\n")]])
 
     def row(self, index):
-        return self.matrix[index-1, :]
+        return list(self.matrix[index-1, :])
 
     def column(self, index):
-        return self.matrix[:, index-1]
-
-
-m = Matrix("1 2 3\n5 56 7\n9 2 6")
-print(m.column(0))
-
-#[print(col) for col in [row.split() for row in s.split("\n")]]
+        return list(self.matrix[:, index-1])

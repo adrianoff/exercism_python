d4228d86ee02318f9d1c79ba7c58188de8db1b58
adrianoff <adrianov87@gmail.com>
2020-06-19 00:38:12 +0300

    isogram

--- hamming/hamming.py
+++ hamming/hamming.py
@@ -1,2 +1,5 @@
 def distance(strand_a, strand_b):
-    pass
+    if len(strand_a) != len(strand_b):
+        raise ValueError('String should be same length.')
+
+    return sum(i != k for i, k in zip(strand_a, strand_b))

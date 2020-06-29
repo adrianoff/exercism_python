07a4222de0ac2a4479b734e037dfda449f91fd39
adrianoff <adrianov87@gmail.com>
2020-06-29 22:03:58 +0300

    acronym

---
+++ acronym/acronym.py
@@ -0,0 +1,7 @@
+import re
+
+
+def abbreviate(words):
+    return ''.join([
+        c[0] for c in words.upper().replace('-', ' ').translate(str.maketrans('', '', '!"#$%&()*+,-./:;<=>?@[\\]^_`{|}~')).split()
+    ])
